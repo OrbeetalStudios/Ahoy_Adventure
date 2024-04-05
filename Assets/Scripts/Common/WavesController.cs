97b79a5b3d069ea3b824628f37453fc96f60994c
newgate21 <124586622+newgate21@users.noreply.github.com>
2024-04-05 23:17:35 +0200

    moved spawn logic to wave controller

--- Assets/Scripts/Common/WavesController.cs
+++ Assets/Scripts/Common/WavesController.cs
@@ -1,22 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using com.cyborgAssets.inspectorButtonPro;
+using MEC;
 
 public class WavesController : MonoBehaviour
 {
-    [SerializeField] private string filenameCsv;
+    [SerializeField, Range(0f, 180f)] private float spawnRadius;
     [SerializeField] private string filenameJson;
-    private List<Dictionary<string, object>> wavesDataCsv = new();
     private JSONWaves wavesDataJson = new();
 
     // Start is called before the first frame update
     void Start()
     {
-        // CSV version
-        wavesDataCsv = CSVParser.Read(filenameCsv);
+        LoadJSON(filenameJson);
+        Timing.RunCoroutine(SpawnEnemy().CancelWith(gameObject));
+    }
 
-        //JSON version
-        wavesDataJson = JSONWaves.CreateFromJSON(TextFileReader.ReadFileAsText(filenameJson));
+    [ProButton]
+    public void LoadJSON(string fileName)
+    {
+        wavesDataJson = JSONWaves.CreateFromJSON(TextFileReader.ReadFileAsText(fileName));
+    }
+    private IEnumerator<float> SpawnEnemy()
+    {
+        while (true)
+        {
+            GameObject enemyShip = PoolController.Instance.GetObjectFromCollection(EPoolObjectType.enemy_default);
+            enemyShip.SetActive(true);
+
+            yield return Timing.WaitForSeconds(8f);
+        }
     }
 }
