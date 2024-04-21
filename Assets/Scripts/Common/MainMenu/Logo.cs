9454cf7f3299975ac361cff7c169c5345c6aeb54
2tefan9 <stef2n9@gmail.com>
2024-04-21 17:47:33 +0200

    Title Scene and LeanTween Plugin

--- Assets/Scripts/Common/MainMenu/Logo.cs
+++ Assets/Scripts/Common/MainMenu/Logo.cs
@@ -6,10 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class Logo : MonoBehaviour
 {
-    public Animator animator;
+    public Animator sprite;
+    public Animator mesh;
+    public GameObject plane;
+    public GameObject spriteObj;
     // Start is called before the first frame update
     public void LoadMenu()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void ActiveFlag()
+    {
+        spriteObj.SetActive(false);
+        plane.SetActive(true);
+    }
+
+    public void DeactiveFlag()
+    {
+        plane.SetActive(false);
+    }
 }
+
