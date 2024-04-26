dc9eb86507f70f8b8ad9d75a368685c1ae2c210e
AlviXXX <alvise.carli.x@gmail.com>
2024-04-26 15:38:33 +0200

    Updated Power-up sprites, modified other parameters

--- Assets/Scripts/Game/Power Up/PowerUpData.cs
+++ Assets/Scripts/Game/Power Up/PowerUpData.cs
@@ -6,7 +6,7 @@ public class PowerUpData : ScriptableObject
     public string ObjectName; // hierarchy name
     public EPowerUpType Type;
     public Sprite img;
-    [Range(0, 15)] public int DurationInSeconds;
+    [Range(0, 60)] public int DurationInSeconds;
     public float Value;
     public bool IsOneShot = false;
     [Range(0f, 10f)] public float spawnChance;
