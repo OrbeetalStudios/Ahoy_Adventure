8503bed173a55af44fda78db9a0b6f69af6aecb8
Davide Ventriglia <davide.ventriglia.swd@gmail.com>
2024-04-26 14:38:07 +0200

    implemented enemy plunder bar

---
+++ Assets/Scripts/Game/Enemy/PlunderBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlunderBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxPlunderTime(int time){
+        slider.maxValue = time;
+        slider.value = time;
+    }
+
+    public void SetPlunderTime(int time){
+        slider.value = time;
+    }
+}
